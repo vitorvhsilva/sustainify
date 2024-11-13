ec2a38cb2a9519a4956c788c8280e66ceeea3642
vitorvhsilva <dndragonbr@gmail.com>
2024-11-13 20:20:52 -0300

    Página de Cadastro feita
    Co-authored-by: Joaogananca26 <joaogananca26@gmail.com>
    Co-authored-by: yamamotolucas <lucasbraccoyamamoto@gmail.com

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,7 +1,8 @@
+import Cadastro from "@/components/LogineCadastro/Cadastro";
 import Login from "@/components/LogineCadastro/Login";
 
 export default function Teste() {
   return (
-    <Login/>
+    <Cadastro/>
   )
 }
--- src/components/LogineCadastro/Cadastro.tsx
+++ src/components/LogineCadastro/Cadastro.tsx
@@ -3,23 +3,40 @@
 import Image from "next/image"
 import Link from "next/link"
 
-export default function Login() {
+export default function Cadastro() {
 
   return (
-    <section className="w-full h-[92vh] bg-cover bg-[url('/login/fundologin.jpg')] flex items-center">
-      <div className="xl:w-[40%] lg:w-[60%] md:w-[80%] w-[95%] h-fit bg-cor1 rounded-xl lg:ml-14 mx-auto">
+    <section className="w-full h-fit py-10 bg-cover bg-[url('/login/fundologin.jpg')] flex items-center lg:justify-end">
+      <div className="xl:w-[40%] lg:w-[60%] md:w-[80%] w-[95%] h-fit bg-cor4 rounded-xl lg:mr-10 mx-auto">
         <Image className="w-48 mx-auto block" src={'/logobranca.png'} alt="Logo da Sustainify" width={1000} height={1000}/>
-        <h1 className="text-center text-corBranca text-3xl mb-2">Login</h1>
-        <h2 className="text-2xl text-corBranca ml-5">Não tem uma conta? <Link href={"/cadastro"} className="text-cor2 underline">Crie uma agora</Link></h2>
+        <h1 className="text-center text-corBranca text-3xl mb-2">Cadastro</h1>
+        <h2 className="text-2xl text-corBranca ml-5">Já tem uma conta? <Link href={"/cadastro"} className="text-cor5 underline">Faça Login</Link></h2>
         <form action="" className="p-5">
+          <div className="w-full h-fit my-5">
+            <label htmlFor="nome" className="block text-2xl text-corBranca font-semibold">Nome</label>
+            <input className="text-2xl px-2 py-2 w-full rounded-xl" type="text" name="nome" id="nome" />
+          </div>
+
           <div className="w-full h-fit my-5">
             <label htmlFor="email" className="block text-2xl text-corBranca font-semibold">Email</label>
             <input className="text-2xl px-2 py-2 w-full rounded-xl" type="text" name="email" id="email" />
           </div>
+
           <div className="w-full h-fit my-5">
             <label htmlFor="senha" className="block text-2xl text-corBranca font-semibold">Senha</label>
             <input className="text-2xl px-2 py-2 w-full rounded-xl" type="text" name="senha" id="senha" />
           </div>
+
+          <div className="w-full h-fit my-5">
+            <label htmlFor="cpf" className="block text-2xl text-corBranca font-semibold">CPF</label>
+            <input className="text-2xl px-2 py-2 w-full rounded-xl" type="text" name="cpf" id="cpf" />
+          </div>
+
+          <div className="w-full h-fit my-5">
+            <label htmlFor="telefone" className="block text-2xl text-corBranca font-semibold">Telefone</label>
+            <input className="text-2xl px-2 py-2 w-full rounded-xl" type="text" name="telefone" id="telefone" />
+          </div>
+
           <h2 className="text-corBranca font-semibold text-2xl mb-2">Tipo de Conta</h2>
           <div className="w-full flex items-center h-fit my-5">
             <div className="w-full h-fit flex items-center md:gap-4 gap-1 ">
@@ -31,7 +48,7 @@ export default function Login() {
               <label htmlFor="sindico" className="text-2xl text-corBranca">Síndico</label>
             </div>
           </div>
-          <input type="submit" className="bg-cor4 hover:bg-cor5 duration-500 transition cursor-pointer px-10 py-2 text-corBranca text-2xl rounded-xl block mx-auto xl:mt-14 mt-6"/>
+          <input type="submit" className="bg-cor1 hover:bg-cor2 duration-500 transition cursor-pointer px-10 py-2 text-corBranca text-2xl rounded-xl block mx-auto xl:mt-14 mt-6"/>
         </form>
       </div>
     </section>
